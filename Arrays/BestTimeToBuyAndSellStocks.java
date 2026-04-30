41aa05055811b503ed92c7d644ae89fd9854be47
Raghav Bhatnagar <raghavbhatnagarixtis@gmail.com>
2026-04-30 17:10:57 +0530

    Create BestTimeToBuyAndSellStocks.java

---
+++ Arrays/BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,24 @@
+// Accepted solution
+package Arrays;
+
+public class BestTimeToBuyAndSellStocks {
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(prices));
+    }
+
+    public static int maxProfit(int[] prices) {
+        int minPrice = Integer.MAX_VALUE; //2147483647
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+
+        return maxProfit;
+    }
+}
